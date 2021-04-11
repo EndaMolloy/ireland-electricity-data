45777bf68ddb849b26d1049335595ada8335b783
Enda <enda.molloy.1@gmail.com>
2021-04-11 13:30:00 +0100

    return df of chosen metric

---
+++ eirgrid_dash.py
@@ -0,0 +1,63 @@
+# Adaptaded from scrapper at tmrowco/electricitymap-contrib
+
+import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
+import requests
+
+
+def params_dict_2_query(query_params_dict, data_type='data'):
+    query_root = f'http://smartgriddashboard.eirgrid.com/DashboardService.svc/{data_type}?'
+    query_params = ''
+
+    for query_param in query_params_dict.keys():
+        query_param_val = query_params_dict[query_param]
+        query_params += f'&{query_param}={query_param_val}'
+
+    query = query_root + query_params
+    return query
+
+def create_query_params_dict(area, region, start_date, end_date):
+    query_params_dict = dict()
+
+    query_params_dict['area'] = area
+    query_params_dict['region'] = region
+    query_params_dict['datefrom'] = start_date
+    query_params_dict['dateto'] = end_date
+
+    return query_params_dict
+
+def format_date_inputs(start_date, end_date):
+    format_dt = lambda dt: datetime.strftime(dt, '%d-%b-%Y+%H:%M').replace(':', '%3A') if isinstance(dt, datetime) else dt
+
+    start_date = format_dt(start_date)
+    end_date = format_dt(end_date)
+
+    return start_date, end_date
+
+def query_API(start_date:datetime, end_date:datetime, area:str, region:str):
+  start_date, end_date = format_date_inputs(start_date, end_date) # Format datetimes
+  query_params_dict = create_query_params_dict(area, region, start_date, end_date) # create dictionary of parameters and their values
+  query = params_dict_2_query(query_params_dict)
+
+  ## Getting & Parsing Response
+  response = requests.get(query)
+  r_json = response.json()
+
+  return r_json
+
+def call_2_df(start_date, end_date, area, region):
+    r_json = query_API(start_date, end_date, area, region) # Making Call
+    df_raw = pd.DataFrame(r_json['Rows']) # Creating DataFrame
+    
+    return df_raw
+
+## User Inputs
+start_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+end_date = start_date + timedelta(days=1) - timedelta(minutes=1)
+area = 'co2Intensity' # ['co2Intensity','co2Emission','pricing','frequency','interconnection', 'windforecast', 'windactual', 'generationactual', 'demandactual','marketdata']
+region = 'ALL' # ['ALL', 'ROI', 'NI']
+
+df_raw = call_2_df(start_date, end_date, area, region)
+
+
