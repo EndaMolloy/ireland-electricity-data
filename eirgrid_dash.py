d3c178ade8a02d8c79bdd181d005d6eb363b1e06
Enda <enda.molloy.1@gmail.com>
2021-05-08 10:30:23 +0100

    push latest data to gsheet

--- eirgrid_dash.py
+++ eirgrid_dash.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import numpy as np
 import requests
+import gspread
 
 
 def params_dict_2_query(query_params_dict, data_type='data'):
@@ -46,18 +47,30 @@ def query_API(start_date:datetime, end_date:datetime, area:str, region:str):
 
   return r_json
 
-def call_2_df(start_date, end_date, area, region):
+def call_2_df(start_date, end_date, area, region, values):
     r_json = query_API(start_date, end_date, area, region) # Making Call
     df_raw = pd.DataFrame(r_json['Rows']) # Creating DataFrame
     
-    return df_raw
+    if values == 'ALL':
+      return df_raw
+    else:
+      return df_raw.dropna().tail(1)
+
+def post_to_gspread(df):
+  gc = gspread.service_account()
+  sh = gc.open("IE_CO2_Intensity")
+  df_list = df.values.tolist()
+  sh.sheet1.append_row(df_list[0], table_range='A1')
+
 
 ## User Inputs
 start_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 end_date = start_date + timedelta(days=1) - timedelta(minutes=1)
 area = 'co2Intensity' # ['co2Intensity','co2Emission','pricing','frequency','interconnection', 'windforecast', 'windactual', 'generationactual', 'demandactual','marketdata']
 region = 'ALL' # ['ALL', 'ROI', 'NI']
+values = 'LAST' # ['ALL','LAST'] All rows or last row
 
-df_raw = call_2_df(start_date, end_date, area, region)
-
+df = call_2_df(start_date, end_date, area, region, values)
+post_to_gspread(df)
+# df.to_csv('IE_co2intensity.csv')
 
